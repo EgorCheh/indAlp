ec6c5cf2febb05681c4d28c4fa1e031ef268eabb
Egich-17 <chehovich.egor@gmail.com>
2021-09-23 10:25:33 +0300

    added new sections

--- index.php
+++ index.php
@@ -28,13 +28,16 @@
 <body>
     <header class="site-header sticky-top py-1">
         <nav class="container d-flex flex-column flex-md-row justify-content-between">
-            <a class="py-2 d-none d-md-inline-block" href="#">О КОМПАНИИ</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
+            <a class="py-2 d-none d-md-inline-block" href="#section_about">О КОМПАНИИ</a>
+            <a class="py-2 d-none d-md-inline-block" href="#section_worktype">ПОТОЛКИ</a>
+            <a class="py-2 d-none d-md-inline-block" href="#section_reviews">ОТЗЫВЫ</a>
+            <a class="py-2 d-none d-md-inline-block" href="#section_questions">ВОПРОСЫ</a>
+            <a class="py-2 d-none d-md-inline-block" href="tel:+3712161166">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                    <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                </svg>
+                +3712161166</a>
+
         </nav>
     </header>
     </div>
@@ -194,7 +197,57 @@
     <div class="parallax">
         <section id="section_why_2">
             <div class="container">
+                <div class="worktype_text">
+                    <h1>НАДЕЖНОЕ СОТРУДНИЧЕСТВО</h1>
+                    <h2 style="color:black">С НАШЕЙ КОМПАНИЕЙ</h2>
+                </div>
+                <div class="container_worktype_cards">
+                    <div class="card" style="width: 18rem;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" color="#FFCE34" fill="currentColor" class="bi bi-check-circle why2" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                        </svg>
+                        <div class="card-body">
+                            <hr>
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 18rem;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" color="#FFCE34" fill="currentColor" class="bi bi-check-circle why2" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                        </svg>
+                        <div class="card-body">
+                            <hr>
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 18rem;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" color="#FFCE34" fill="currentColor" class="bi bi-check-circle why2" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                        </svg>
+                        <div class="card-body">
+                            <hr>
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 18rem;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" color="#FFCE34" fill="currentColor" class="bi bi-check-circle why2" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                        </svg>
+                        <div class="card-body">
+                            <hr>
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
 
+                </div>
             </div>
         </section>
     </div>
@@ -202,6 +255,70 @@
     <div class="parallax">
         <section id="section_questions">
             <div class="container">
+                <div class="worktype_text">
+                    <h1>ЧАСТО ЗАДАВАЕМЫЕ</h1>
+                    <h2 style="color:black">ВОПРОСЫ КЛИЕНТОВ</h2>
+                </div>
+                <div class="container_worktype_cards row row-cols-1 row-cols-md-2 g-4">
+
+                    <div class="col">
+                        <div class="card" style="width: 36rem;">
+                            <div class="card-head">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" color="#FFCE34" fill="currentColor" class="bi bi-check-circle " viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                                </svg>
+                                <h5 class="card-title">Сколько лет существует компания?</h5>
+                            </div>
+
+                            <p class="card-text">Наша компания работает на рынке услуг уже 20 лет. Мы работаем с клиентами из России и бывших стран СНГ. Самым важным для нас всегда будет добиться результата, который хочет получить наш клиент, сотрудничая с нашей компанией. </p>
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" style="width: 36rem;">
+                            <div class="card-head">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" color="#FFCE34" fill="currentColor" class="bi bi-check-circle " viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                                </svg>
+                                <h5 class="card-title">Сколько лет существует компания?</h5>
+                            </div>
+
+                            <p class="card-text">Наша компания работает на рынке услуг уже 20 лет. Мы работаем с клиентами из России и бывших стран СНГ. Самым важным для нас всегда будет добиться результата, который хочет получить наш клиент, сотрудничая с нашей компанией. </p>
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" style="width: 36rem;">
+                            <div class="card-head">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" color="#FFCE34" fill="currentColor" class="bi bi-check-circle " viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                                </svg>
+                                <h5 class="card-title">Сколько лет существует компания?</h5>
+                            </div>
+
+                            <p class="card-text">Наша компания работает на рынке услуг уже 20 лет. Мы работаем с клиентами из России и бывших стран СНГ. Самым важным для нас всегда будет добиться результата, который хочет получить наш клиент, сотрудничая с нашей компанией. </p>
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" style="width: 36rem;">
+                            <div class="card-head">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" color="#FFCE34" fill="currentColor" class="bi bi-check-circle " viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                                </svg>
+                                <h5 class="card-title">Сколько лет существует компания?</h5>
+                            </div>
+
+                            <p class="card-text">Наша компания работает на рынке услуг уже 20 лет. Мы работаем с клиентами из России и бывших стран СНГ. Самым важным для нас всегда будет добиться результата, который хочет получить наш клиент, сотрудничая с нашей компанией. </p>
+
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
         </section>
@@ -211,7 +328,10 @@
     <div class="parallax">
         <section id="section_reviews">
             <div class="container">
-
+                <div class="worktype_text">
+                    <h1>ОТЗЫВЫ</h1>
+                    <h2 style="color:black">НАШИХ КЛИЕНТОВ</h2>
+                </div>
             </div>
         </section>
     </div>
@@ -219,7 +339,24 @@
     <div class="parallax">
         <section id="section_offer">
             <div class="container">
+                <div class="container_form" > <form>
+                <div class="worktype_text">
+                    <h2 style="color:black">ПОЗВОНИТЕ СЕЙЧАС И ПОЛУЧИТЕ</h2>
+                    <h1>СКИДКУ 15%!</h1><br>
+                <div class="mb-3">
 
+                            <input type="text" class="form-control" placeholder="Имя" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <input type="tel" class="form-control" placeholder="Телефон" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3">
+                            <input type="mail" class="form-control" placeholder="Е-mail" id="exampleInputPassword1">
+                        </div>
+                        <button type="submit" class="btn btn-secondary" style=" background-color: #353333; border-color: #353333;;">Отправить заявку</button>
+                </form>
+            </div>
+               
 
             </div>
         </section>
